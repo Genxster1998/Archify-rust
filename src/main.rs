295f67cd60566a2c52a1dd9ccbcc179164905a60
Genxster1998 <ck.2229.ck@gmail.com>
2026-07-02 23:30:09 +0530

    Reduce initial window width to the minimum of 400.0

--- src/main.rs
+++ src/main.rs
@@ -72,7 +72,7 @@ fn main() -> Result<(), eframe::Error> {
 
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([450.0, 770.0])
+            .with_inner_size([400.0, 770.0])
             .with_min_inner_size([400.0, 370.0])
             .with_icon(std::sync::Arc::new(icon.unwrap_or_default())),
         ..Default::default()
